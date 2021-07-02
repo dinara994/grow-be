a80e9dd211f42c5f3a35a52e2ad5c9e8d5f85d24
dinara994 <emailaddress@yahoo.com>
2021-07-02 08:24:03 +0600

    initial

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        margin: 30,
+        loop: true,
+        stagePadding: 300,
+        center: true
+    });
+});
